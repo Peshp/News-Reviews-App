4774d8aab1394c28b082dd8c373b79ef5918bfd7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-20 14:03:55 +0300

    Create project
    Create project for softuni

---
+++ Project/News&Reviews/News&Reviews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace News_Reviews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
