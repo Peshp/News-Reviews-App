97a37256437e78727c1c33f3b2ac01d11585e1a5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-25 18:49:25 +0300

    Seed database

---
+++ Project/News&Reviews/News&Reviews.Data/Configuration/CityEntityConfigurator.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using News_Reviews.DataModels.DataModels;
+
+namespace News_Reviews.Data.Configuration
+{
+    public class CityEntityConfigurator : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasData(this.GenerateCities());
+        }
+
+        private City[] GenerateCities()
+        {
+            ICollection<City> cities = new HashSet<City>();
+
+            City city;
+
+            city = new City()
+            {
+                Id = 1,
+                Name = "Paris",
+                CountryId = 1,
+            };
+            cities.Add(city);
+
+            city = new City()
+            {
+                Id = 2,
+                Name = "Tokio",
+                CountryId = 2,
+            };
+            cities.Add(city);
+
+            city = new City()
+            {
+                Id = 3,
+                Name = "London",
+                CountryId = 3,
+            };
+            cities.Add(city);
+
+            city = new City()
+            {
+                Id = 4,
+                Name = "Paris",
+                CountryId = 1,
+            };
+            cities.Add(city);
+
+            city = new City()
+            {
+                Id = 5,
+                Name = "Warsaw",
+                CountryId = 5,
+            };
+            cities.Add(city);
+
+            return cities.ToArray();
+        }
+    }
+}
