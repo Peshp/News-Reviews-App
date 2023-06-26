0266dd98fe79f99e7bcbaaa28d305959d89f6bf4
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-26 13:32:29 +0300

    Seeding publisher entity

---
+++ Project/News&Reviews/News&Reviews.Data/Configuration/PublisherEntityConfigurator.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using News_Reviews.DataModels.DataModels;
+
+namespace News_Reviews.Data.Configuration
+{
+    public class PublisherEntityConfigurator : IEntityTypeConfiguration<Publisher>
+    {
+        public void Configure(EntityTypeBuilder<Publisher> builder)
+        {
+            builder.HasData(this.GeneratePublishers());
+        }
+
+        private Publisher[] GeneratePublishers()
+        {
+            ICollection<Publisher> publishers = new HashSet<Publisher>();
+
+            Publisher publisher = null;
+
+            publisher = new Publisher()
+            {
+                Id = 1,
+                Name = "Ubisoft",
+                CountryId = 1,
+                CityId = 1,
+            };
+            publishers.Add(publisher);
+
+            publisher = new Publisher()
+            {
+                Id = 2,
+                Name = "EA",
+                CountryId = 4,
+                CityId = 4,
+            };
+            publishers.Add(publisher);
+
+            publisher = new Publisher()
+            {
+                Id = 3,
+                Name = "Sony",
+                CountryId = 2,
+                CityId = 2,
+            };
+            publishers.Add(publisher);
+
+            publisher = new Publisher()
+            {
+                Id = 4,
+                Name = "From software",
+                CountryId = 2,
+                CityId = 2,
+            };
+            publishers.Add(publisher);
+
+            publisher = new Publisher()
+            {
+                Id = 5,
+                Name = "CDPR",
+                CountryId = 5,
+                CityId = 5,
+            };
+            publishers.Add(publisher);
+
+            return publishers.ToArray();
+        }
+    }
+}
