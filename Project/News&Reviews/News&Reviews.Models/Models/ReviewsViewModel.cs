a3d8d99dfaa170b45eecd80e438c7eb43446375b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-27 11:40:23 +0300

    Make ReviewsViewModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/ReviewsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace News_Reviews.Models.Models
+{
+    public class ReviewsViewModel
+    {    
+        public int Id { get; set; }
+
+        public string Game { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ImageURL { get; set; }
+       
+    }
+}
