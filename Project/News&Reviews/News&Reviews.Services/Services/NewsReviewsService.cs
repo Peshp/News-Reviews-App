bb589746aa37d39b08314f03b01e4a1ba97daf03
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-06-27 11:42:48 +0300

    Fill data from Reviews context and ReviewsViewModel

--- Project/News&Reviews/News&Reviews.Services/Services/NewsReviewsService.cs
+++ Project/News&Reviews/News&Reviews.Services/Services/NewsReviewsService.cs
@@ -1,6 +1,29 @@
-﻿namespace News_Reviews.Services.Services
+﻿using Microsoft.EntityFrameworkCore;
+using News_Reviews.Data;
+using News_Reviews.Models.Models;
+using News_Reviews.Services.Interfaces;
+
+namespace News_Reviews.Services.Services
 {
-    public class NewsReviewsService
+    public class NewsReviewsService : INewsReviewsInterface
     {
+        private readonly ApplicationDbContext context;
+
+        public async Task<IEnumerable<ReviewsViewModel>> GetReviewsAsync()
+        {
+            var models = await context.Reviews.Include(g => g.Game).ToArrayAsync();
+
+            var reviews = models
+                .Select(r => new ReviewsViewModel
+                {
+                    Id = r.Id,
+                    Game = r.Game.Name,
+                    Title = r.Title,
+                    Content = r.Content,
+                    ImageURL = r.ImageURL,
+                });
+
+            return reviews;
+        }
     }
 }
