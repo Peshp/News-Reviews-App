43cdbad0b2e6d313a04f99431436951c2c4ff12a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-01 17:44:45 +0300

    Add news form model

---
+++ Project/News&Reviews/News&Reviews.Models/Models/NewsFormModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.ReviewsConstants;
+using static News_Reviews.Common.Constants.ContentConstats;
+
+namespace News_Reviews.Models.Models
+{
+    public class NewsFormModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+
+        [Required]
+        public string Data { get; set; }
+
+        [Required]
+        public int PlatformId { get; set; }
+
+        public IEnumerable<PlatformViewModel> Platforms { get; set; }
+    }
+}
