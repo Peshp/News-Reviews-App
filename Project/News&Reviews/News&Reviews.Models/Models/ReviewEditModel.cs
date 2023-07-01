224b738a3bb8643a9de1e03946a866b5b3aa565c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-01 17:44:55 +0300

    Change ReviewsFormModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/ReviewEditModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.ReviewsConstants;
+using static News_Reviews.Common.Constants.ContentConstats;
+
+namespace News_Reviews.Models.Models
+{
+    public class ReviewEditModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Game { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+
+        [Required]
+        public string ImageURL { get; set; }
+    }
+}
