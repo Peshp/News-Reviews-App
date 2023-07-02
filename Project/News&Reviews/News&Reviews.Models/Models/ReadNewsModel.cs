7ff5f17fa91788ab73713bbea7e65048bc5043e4
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-02 15:22:59 +0300

    Make readnewsmodel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/ReadNewsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace News_Reviews.Models.Models
+{
+    public class ReadNewsModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
