3e53f0f1d2c4dbe7a473132bcf0189def04c1810
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-02 15:23:21 +0300

    Configurate Inewsservice

--- Project/News&Reviews/News&Reviews.Services/Interfaces/INewsService.cs
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/INewsService.cs
@@ -9,5 +9,13 @@ namespace News_Reviews.Services.Interfaces
         public Task AddNews(NewsFormModel model);
 
         public Task<IEnumerable<PlatformViewModel>> GetPlatformAsync();
+
+        public Task<ReadNewsModel> ReadNews(int id);
+
+        public Task DeleteNews(int id);
+
+        public Task<NewsEditModel> FindNewsById(int id);
+
+        public Task EditNewsAsync(int id, NewsFormModel model);
     }
 }
