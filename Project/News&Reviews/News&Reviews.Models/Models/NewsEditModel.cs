d7c1404f61d42defa711630601389723bd97a716
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-02 15:22:45 +0300

    Meke newsedit model

---
+++ Project/News&Reviews/News&Reviews.Models/Models/NewsEditModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.ReviewsConstants;
+using static News_Reviews.Common.Constants.ContentConstats;
+
+namespace News_Reviews.Models.Models
+{
+    public class NewsEditModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
