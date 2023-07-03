24742476f72624be7a57055dd84cea921f537fed
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-03 18:51:19 +0300

    Create new entity comment

---
+++ Project/News&Reviews/News&Reviews.DataModels/Comment.cs
@@ -0,0 +1,22 @@
+﻿using News_Reviews.Common.UserConstants;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static News_Reviews.Common.Constants.CommentConstants;
+
+namespace News_Reviews.DataModels
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
