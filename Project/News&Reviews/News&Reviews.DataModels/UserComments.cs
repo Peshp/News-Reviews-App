60b4ac70f9673bf5cdefe6d664c3bb70ae9ed15e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-03 18:51:40 +0300

    Create many to many table for comments and users

---
+++ Project/News&Reviews/News&Reviews.DataModels/UserComments.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace News_Reviews.DataModels
+{
+    public class UserComments
+    {
+        [ForeignKey(nameof(ApplicationUser))]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
