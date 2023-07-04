6b287cdc4905b983ecc203b4a5efeca0e5ca6614
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-04 14:10:46 +0300

    Create CommentsFormModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Comments/CommentsFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.CommentConstants;
+
+namespace News_Reviews.Models.Models.Comments
+{
+    public class CommentsFormModel
+    {
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
