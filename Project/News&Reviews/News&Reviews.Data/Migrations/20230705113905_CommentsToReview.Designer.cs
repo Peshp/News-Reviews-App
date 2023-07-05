16dd45f90b5363d497ec244e7a5495c8b8e4ba76
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-05 14:53:05 +0300

    Update database

--- Project/News&Reviews/News&Reviews.Data/Migrations/20230705113905_CommentsToReview.Designer.cs
+++ Project/News&Reviews/News&Reviews.Data/Migrations/20230705113905_CommentsToReview.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using News_Reviews.Data;
 
@@ -11,9 +12,10 @@ using News_Reviews.Data;
 namespace News_Reviews.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230705113905_CommentsToReview")]
+    partial class CommentsToReview
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Project/News&Reviews/News&Reviews.Data/Migrations/20230705113905_CommentsToReview.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News_Reviews.Data.Migrations
+{
+    public partial class CommentsToReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Reviews_ReviewId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ReviewId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewId",
+                table: "Comments");
+
+            migrationBuilder.CreateTable(
+                name: "CommentReview",
+                columns: table => new
+                {
+                    CommentsId = table.Column<int>(type: "int", nullable: false),
+                    ReviewsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommentReview", x => new { x.CommentsId, x.ReviewsId });
+                    table.ForeignKey(
+                        name: "FK_CommentReview_Comments_CommentsId",
+                        column: x => x.CommentsId,
+                        principalTable: "Comments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CommentReview_Reviews_ReviewsId",
+                        column: x => x.ReviewsId,
+                        principalTable: "Reviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentReview_ReviewsId",
+                table: "CommentReview",
+                column: "ReviewsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommentReview");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReviewId",
+                table: "Comments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ReviewId",
+                table: "Comments",
+                column: "ReviewId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Reviews_ReviewId",
+                table: "Comments",
+                column: "ReviewId",
+                principalTable: "Reviews",
+                principalColumn: "Id");
+        }
+    }
+}
