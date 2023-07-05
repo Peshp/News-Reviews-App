2da7868a7ca4ea245c4258c44b75f5544b2862d2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-05 14:53:22 +0300

    update netity

--- Project/News&Reviews/News&Reviews.DataModels/Comment.cs
+++ Project/News&Reviews/News&Reviews.DataModels/Comment.cs
@@ -1,4 +1,5 @@
 ﻿using News_Reviews.Common.UserConstants;
+using News_Reviews.DataModels.DataModels;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static News_Reviews.Common.Constants.CommentConstants;
@@ -18,5 +19,7 @@ namespace News_Reviews.DataModels
         public string UserId { get; set; }
 
         public virtual ApplicationUser User { get; set; }
+
+        public IEnumerable<Review> Reviews { get; set; } = new List<Review>();
     }
 }
