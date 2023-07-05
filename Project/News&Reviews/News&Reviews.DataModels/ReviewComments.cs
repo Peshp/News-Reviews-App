bcc88b5c879bf8f62619be70b78f1bf22af4ff31
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-05 14:53:32 +0300

    add new many to many table

---
+++ Project/News&Reviews/News&Reviews.DataModels/ReviewComments.cs
@@ -0,0 +1,16 @@
+﻿using News_Reviews.DataModels.DataModels;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace News_Reviews.DataModels
+{
+    public class ReviewComments
+    {
+        [ForeignKey(nameof(Review))]
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
