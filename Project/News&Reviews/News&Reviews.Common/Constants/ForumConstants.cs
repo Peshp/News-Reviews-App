c86827b0fb45dbd036e2a465e269d069b50ce366
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-07 21:39:25 +0300

    Make new constants for forum

---
+++ Project/News&Reviews/News&Reviews.Common/Constants/ForumConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace News_Reviews.Common.Constants
+{
+    public static class ForumConstants
+    {
+        public const int PostContentMaxLength = 1000;
+        public const int PostContentMinLength = 10;
+
+        public const int ThemeTitleMaxLength = 3;
+        public const int ThemeTitleMinLength = 50;
+    }
+}
