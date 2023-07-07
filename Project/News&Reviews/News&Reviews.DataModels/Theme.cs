2d162f29570ce08c64c7d9ed2e8ebec151182618
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-07 21:40:42 +0300

    Make Theme entity

---
+++ Project/News&Reviews/News&Reviews.DataModels/Theme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.ForumConstants;
+
+namespace News_Reviews.DataModels
+{
+    public class Theme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ThemeTitleMaxLength)]
+        public string Title { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; } = new List<Post>();
+    }
+}
