1b3f3711a3c87b3878364f6276eb251928832911
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-07 21:40:33 +0300

    Make Post entity

---
+++ Project/News&Reviews/News&Reviews.DataModels/Post.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static News_Reviews.Common.Constants.ForumConstants;
+
+namespace News_Reviews.DataModels
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(PostContentMaxLength)]
+        public string Content { get; set; }
+
+        [ForeignKey(nameof(ApplicationUser))]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Theme))]
+        public int ThemeId { get; set; }
+
+        public Theme Theme { get; set; }
+    }
+}
