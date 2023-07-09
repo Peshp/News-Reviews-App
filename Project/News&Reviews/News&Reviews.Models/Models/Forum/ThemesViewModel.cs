3073e2e1754d88929fcba22c8f5ecd15bda55054
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-09 14:36:16 +0300

    Make ThemeViewModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Forum/ThemesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace News_Reviews.Models.Models.Forum
+{
+    public class ThemesViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public IEnumerable<PostViewModel> Posts { get; set; } = new List<PostViewModel>();
+    }
+}
