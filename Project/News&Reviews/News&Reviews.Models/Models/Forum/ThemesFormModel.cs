cdca49c8051c1db529ed0994204faff767ba8488
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-09 14:36:05 +0300

    Make ThemesFormModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Forum/ThemesFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.ForumConstants;
+
+namespace News_Reviews.Models.Models.Forum
+{
+    public class ThemesFormModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(ThemeTitleMaxLength, MinimumLength = ThemeTitleMinLength)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
