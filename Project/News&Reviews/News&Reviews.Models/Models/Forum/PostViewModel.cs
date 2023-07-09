2978b5ff8e882bb362e2b8e46ec81fc570f57222
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-09 14:35:43 +0300

    Make PostViewModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Forum/PostViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace News_Reviews.Models.Models.Forum
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public int ThemeId { get; set; }
+    }
+}
