32c693bb2693ecf0c1c56a464c18c35e71b3b30a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-09 14:36:53 +0300

    Create IForumService interface

---
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/IForumService.cs
@@ -0,0 +1,15 @@
+﻿using News_Reviews.Models.Models.Forum;
+
+namespace News_Reviews.Services.Interfaces
+{
+    public interface IForumService
+    {
+        Task<IEnumerable<ThemesViewModel>> GetThemesAsync();
+
+        Task<IEnumerable<PostViewModel>> GetPostsAsync(int themeId, string username);
+
+        Task AddNewThemeAsync(ThemesFormModel model);
+
+        Task RemoveThemeAsync(int themeId);
+    }
+}
