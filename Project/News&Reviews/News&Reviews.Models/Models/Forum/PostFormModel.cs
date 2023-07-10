08052538babf3d7741c4d92c4ff2ddc0d1b91044
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-10 23:05:15 +0300

    Modify PostFormModel

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Forum/PostFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.ForumConstants;
+
+namespace News_Reviews.Models.Models.Forum
+{
+    public class PostFormModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(PostContentMaxLength, MinimumLength = PostContentMinLength)]
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+
+        public int ThemeId { get; set; }
+    }
+}
