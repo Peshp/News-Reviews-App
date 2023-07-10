87668ccfe921ef1cbb11d08b516422536b3974ed
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-10 23:05:42 +0300

    Modify interface

--- Project/News&Reviews/News&Reviews.Services/Interfaces/IForumService.cs
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/IForumService.cs
@@ -4,12 +4,16 @@ namespace News_Reviews.Services.Interfaces
 {
     public interface IForumService
     {
-        Task<IEnumerable<ThemesViewModel>> GetThemesAsync();
+        Task<IEnumerable<ThemesViewModel>> GetThemesAsync(IEnumerable<PostViewModel> posts);
 
         Task<IEnumerable<PostViewModel>> GetPostsAsync(int themeId, string username);
 
         Task AddNewThemeAsync(ThemesFormModel model);
 
         Task RemoveThemeAsync(int themeId);
+
+        Task AddNewPostAsync(PostViewModel model, string username, int themeId);
+
+        Task RemovePostAsync(int postId);
     }
 }
