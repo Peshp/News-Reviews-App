ae965ac3c806ff0d4251b471d7f0c906710e96c1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-14 22:06:57 +0300

    Create UserAnswers.cs

---
+++ Project/News&Reviews/News&Reviews.DataModels/UserAnswers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace News_Reviews.DataModels
+{
+    public class UserAnswers
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        [ForeignKey(nameof(Question))]
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+
+        [ForeignKey(nameof(Answer))]
+        public int? AnswerId { get; set; }
+        public Answer Answer { get; set; }
+    }
+}
