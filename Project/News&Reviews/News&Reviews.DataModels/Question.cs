7704957ebc13120329602a74f97139cea876e476
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-14 22:06:50 +0300

    Create Question.cs

---
+++ Project/News&Reviews/News&Reviews.DataModels/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static News_Reviews.Common.Constants.QuizConstants;
+
+namespace News_Reviews.DataModels
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(QuestionTitleMaxLength)]
+        public string Title { get; set; }
+
+        public IEnumerable<Answer> Answers { get; set; } = new HashSet<Answer>();
+
+        public IEnumerable<UserAnswers> UserAnswers { get; set; } = new HashSet<UserAnswers>();
+    }
+}
