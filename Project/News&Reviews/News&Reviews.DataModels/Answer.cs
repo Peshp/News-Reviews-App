1d049b38c4024c75fa9a4b7a60681f7d7552d8dd
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-14 22:06:46 +0300

    Create Answer.cs

---
+++ Project/News&Reviews/News&Reviews.DataModels/Answer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static News_Reviews.Common.Constants.QuizConstants;
+
+namespace News_Reviews.DataModels
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength()]
+        public string Title { get; set; }
+
+        public int Points { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Question))]
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+
+        public IEnumerable<UserAnswers> UserAnswers { get; set; } = new HashSet<UserAnswers>();
+    }
+}
