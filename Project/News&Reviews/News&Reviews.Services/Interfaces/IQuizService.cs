2c77ae3583e717bfbb44ca6f15c9891d1491ef2a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-15 18:39:22 +0300

    Create IQuizService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/IQuizService.cs
@@ -0,0 +1,11 @@
+﻿using News_Reviews.Models.Models.Quzi;
+
+namespace News_Reviews.Services.Interfaces
+{
+    public interface IQuizService
+    {
+        //Task<IEnumerable<AnswersViewModel>> GetAnswers();
+
+        Task<IEnumerable<QuestionsViewModel>> GetQuestions();
+    }
+}
