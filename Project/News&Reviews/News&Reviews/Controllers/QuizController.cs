29556eb60c4e9fe092527a37636cfb58b1ada4a7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-15 18:39:37 +0300

    Create QuizController.cs

---
+++ Project/News&Reviews/News&Reviews/Controllers/QuizController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News_Reviews.Services.Interfaces;
+using News_Reviews.Services.Services;
+
+namespace News_Reviews.Controllers
+{
+    public class QuizController : Controller
+    {
+        private readonly IQuizService service;
+
+        public QuizController(IQuizService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var questions = await service.GetQuestions();           
+
+            return View(questions);
+        }
+    }
+}
