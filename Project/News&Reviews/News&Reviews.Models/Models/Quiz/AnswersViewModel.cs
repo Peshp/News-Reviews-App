73938e12d29839e7fad6bd926bba7ece21585ef8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-15 18:39:10 +0300

    Create AnswersViewModel.cs

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Quiz/AnswersViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace News_Reviews.Models.Models.Quzi
+{
+    public class AnswersViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Points { get; set; }
+
+        public int QuestionId { get; set; }
+    }
+}
