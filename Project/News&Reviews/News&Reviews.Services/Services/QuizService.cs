98cacae83e2b58ef2e01e991b2f709ad6f322e3a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-15 18:39:28 +0300

    Create QuizService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Services/QuizService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using News_Reviews.Data;
+using News_Reviews.Models.Models.Quzi;
+using News_Reviews.Services.Interfaces;
+using System.Xml.Linq;
+
+namespace News_Reviews.Services.Services
+{
+    public class QuizService : IQuizService
+    {
+        private readonly ApplicationDbContext context;
+
+        public QuizService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<QuestionsViewModel>> GetQuestions()
+        {
+            var models = await context.Questions
+                .Include(q => q.Answers)
+                .ToArrayAsync();
+
+            var questions = models
+                .Select(q => new QuestionsViewModel
+                {
+                    Id = q.Id,
+                    Title = q.Title,
+                    Answers = q.Answers.Select(a => new AnswersViewModel
+                    {
+                        Id = a.Id,
+                        Title = a.Title,
+                        Points = a.Points,
+                        QuestionId = q.Id,
+                    })
+                    .ToArray(),
+                }); 
+
+            return questions;
+        }
+    }
+}
