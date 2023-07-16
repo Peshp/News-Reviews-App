36dd70c5a7671abc19fb6f4a17b11f1c1acc5860
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-16 13:10:03 +0300

    Create 20230715203408_ChangeConfigurator.cs

---
+++ Project/News&Reviews/News&Reviews.Data/Migrations/20230715203408_ChangeConfigurator.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News_Reviews.Data.Migrations
+{
+    public partial class ChangeConfigurator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Title",
+                value: "Mostly action, I guess.");
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Title",
+                value: "I like logic, such as simple puzzles.");
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Title",
+                value: "A good balance of both");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Title",
+                value: "A good balance of both");
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Title",
+                value: "Mostly action, I guess.");
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Title",
+                value: "I like logic, such as simple puzzles.");
+        }
+    }
+}
