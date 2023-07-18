c140d3cc747877f13ac04f2a15f70d37b8e13537
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-18 13:53:46 +0300

    Create 20230718104358_updateCommentEntity.cs

---
+++ Project/News&Reviews/News&Reviews.Data/Migrations/20230718104358_updateCommentEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News_Reviews.Data.Migrations
+{
+    public partial class updateCommentEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Comments");
+        }
+    }
+}
