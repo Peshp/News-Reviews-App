08c80723b7c0a451c2801f4262ef05194729ef82
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-20 17:06:13 +0300

    Update News&ReviewsDbContext.cs

--- Project/News&Reviews/News&Reviews.Data/News&ReviewsDbContext.cs
+++ Project/News&Reviews/News&Reviews.Data/News&ReviewsDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using News_Reviews.DataModels;
 using News_Reviews.DataModels.DataModels;
