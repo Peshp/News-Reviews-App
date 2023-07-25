623b4e6f0c839652a988ddc108ffe3c78f534010
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-25 11:16:35 +0300

    Update ReadReviewModel.cs

--- Project/News&Reviews/News&Reviews.Models/Models/Reviews/ReadReviewModel.cs
+++ Project/News&Reviews/News&Reviews.Models/Models/Reviews/ReadReviewModel.cs
@@ -11,6 +11,8 @@ namespace News_Reviews.Models.Models.Reviews
 
         public int Id { get; set; }
 
+        public string ImageUrl { get; set; }
+
         public string Title { get; set; }
 
         public string Content { get; set; }
