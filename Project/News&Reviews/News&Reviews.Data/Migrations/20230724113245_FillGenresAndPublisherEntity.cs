4ebae4bd4c5365e7a03641d67c6709a9aa33d786
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-26 11:52:03 +0300

    Create 20230724113245_FillGenresAndPublisherEntity.cs

---
+++ Project/News&Reviews/News&Reviews.Data/Migrations/20230724113245_FillGenresAndPublisherEntity.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News_Reviews.Data.Migrations
+{
+    public partial class FillGenresAndPublisherEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 7, "Platform" },
+                    { 8, "Fighting" },
+                    { 9, "Survival" },
+                    { 10, "Battle Royale" },
+                    { 11, "Action-adventure" },
+                    { 12, "Visual novels" },
+                    { 13, "Simulator" },
+                    { 14, "Racing" },
+                    { 15, "Strategy" },
+                    { 16, "Sports" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 6, "Techland" },
+                    { 7, "Blizzard" },
+                    { 8, "Rockstar" },
+                    { 9, "Capcom" },
+                    { 10, "Microsoft" },
+                    { 11, "Square Enix" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 11);
+        }
+    }
+}
