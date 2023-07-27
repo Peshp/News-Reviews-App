85b3ce56bc795a44e3076d904b68610e1f45a84c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-27 13:11:37 +0300

    Create DisplayProfileViewModel.cs

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Account/DisplayProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using News_Reviews.Models.Models.Comments;
+using News_Reviews.Models.Models.Forum;
+
+namespace News_Reviews.Models.Models.Account
+{
+    public class DisplayProfileViewModel
+    {
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
