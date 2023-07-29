8c14f293230de58244c81b6b486c0a90862d19b9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-29 10:29:09 +0300

    Update AdminController.cs

--- Project/News&Reviews/News&Reviews/Areas/Administration/Controllers/AdminController.cs
+++ Project/News&Reviews/News&Reviews/Areas/Administration/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using News_Reviews.Areas.Administration.Models;
+using News_Reviews.Data;
 using News_Reviews.DataModels;
 
 namespace News_Reviews.Areas.Administration.Controllers
@@ -11,13 +13,43 @@ namespace News_Reviews.Areas.Administration.Controllers
     {
         private UserManager<ApplicationUser> userManager;
         private SignInManager<ApplicationUser> signInManager;
+        private ApplicationDbContext context;
 
         public AdminController(
             UserManager<ApplicationUser> _userManager,
-            SignInManager<ApplicationUser> _signInManager)
+            SignInManager<ApplicationUser> _signInManager,
+            ApplicationDbContext context)
         {
             userManager = _userManager;
             signInManager = _signInManager;
+            this.context = context;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var model = new List<ApplicationUserModel>();
+            ApplicationUserModel mod;
+            var roleId = "0a1c1685-c05d-4186-a1a0-fbaebdb7ae79"; 
+
+            var usersWithRole = context.UserRoles
+                .Where(r => r.RoleId == roleId);
+
+            foreach (var userRole in usersWithRole)
+            {
+                var user = await userManager.FindByIdAsync(userRole.UserId);
+
+                if (user != null)
+                {
+                    mod = new ApplicationUserModel
+                    {
+                        Id = user.Id,
+                        Name = user.UserName,
+                    };
+                    model.Add(mod);
+                }
+            }
+
+            return View(model);
         }
 
         public async Task<IActionResult> MakeMod(string username)
@@ -31,7 +63,7 @@ namespace News_Reviews.Areas.Administration.Controllers
 
             var result = await userManager.AddToRoleAsync(user, "Moderator");
 
-            return Ok($"Role moderator is successfull added to {username}");
+            return Ok($"Role moderator is successfull added to {"Tonkata2"}");
         }
     }
 }
