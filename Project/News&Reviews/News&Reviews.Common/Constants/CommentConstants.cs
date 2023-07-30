79b8abf41ccc63ef4a8c104cdc239c9a70a2e653
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-07-30 18:29:10 +0300

    Update CommentConstants.cs

--- Project/News&Reviews/News&Reviews.Common/Constants/CommentConstants.cs
+++ Project/News&Reviews/News&Reviews.Common/Constants/CommentConstants.cs
@@ -3,6 +3,6 @@
     public static class CommentConstants
     {
         public const int ContentMinLength = 10;
-        public const int ContentMaxLength = 500;
+        public const int ContentMaxLength = 1000;
     }
 }
