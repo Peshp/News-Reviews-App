ea78f3bdaf7e529efb8cfda897f77c975676c993
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-02 16:47:01 +0300

    Create SeedInMemoryData.cs

---
+++ Project/News&Reviews/News&Reviews.Tests/SeedInMemoryData.cs
@@ -0,0 +1,25 @@
+﻿using News_Reviews.Data;
+using News_Reviews.DataModels;
+
+namespace News_Reviews.Tests
+{
+    public class SeedInMemoryData
+    {
+        public static void SeedUsers(ApplicationDbContext context)
+        {
+            var user = new ApplicationUser
+            {
+                Id = "40d45d54-4e27-4c4c-b751-0fff188c021d",
+                UserName = "pesho",
+                NormalizedUserName = "PESHO",
+                PasswordHash = "AQAAAAEAACcQAAAAEF3HewxMQfh5W03TxONHudxZ9Gyzkw5JJkAlWjSPS+ByyZGGVGXMh4TCSgtmFU9+7Q==",
+                SecurityStamp = "MOL5YNZIDEIMR65ECH5SRG2UDMPFKEIW",
+                ConcurrencyStamp = "d3ee842b-1be4-4dc2-96f9-faf06b17941c",
+                LockoutEnabled = true,
+            };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+    }
+}
