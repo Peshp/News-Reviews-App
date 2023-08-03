db5a77624984366eeb6a04176bce2df8650a1460
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-03 09:51:09 +0300

    Create ISearchService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/ISearchService.cs
@@ -0,0 +1,13 @@
+﻿using News_Reviews.DataModels.DataModels;
+using News_Reviews.Models.Models.News;
+using News_Reviews.Models.Models.Reviews;
+
+namespace News_Reviews.Services.Interfaces
+{
+    public interface ISearchService
+    {
+        Task<IEnumerable<ReviewsViewModel>> SearchReview(string query, IEnumerable<ReviewsViewModel> reviews);
+
+        Task<IEnumerable<NewsViewModel>> SearchNews(string query, IEnumerable<NewsViewModel> news);
+    }
+}
