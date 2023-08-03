d2ebe5c7aa5e0d0cd98606e7f8fa5d5b2ba5f8c4
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-03 09:51:11 +0300

    Create SearchService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Services/SearchService.cs
@@ -0,0 +1,31 @@
+﻿using News_Reviews.DataModels.DataModels;
+using News_Reviews.Models.Models.News;
+using News_Reviews.Models.Models.Reviews;
+using News_Reviews.Services.Interfaces;
+using System;
+
+namespace News_Reviews.Services.Services
+{
+    public class SearchService : ISearchService
+    {
+        public async Task<IEnumerable<NewsViewModel>> SearchNews(string query, IEnumerable<NewsViewModel> news)
+        {
+            if(query != null)
+            {
+                news = news.Where(n => n.Title.Contains(query, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return news;
+        }
+
+        public async Task<IEnumerable<ReviewsViewModel>> SearchReview(string query, IEnumerable<ReviewsViewModel> reviews)
+        {
+            if (query != null)
+            {
+                reviews = reviews.Where(r => r.Title.Contains(query, StringComparison.OrdinalIgnoreCase));
+            }
+            
+            return reviews;
+        }
+    }
+}
