e54167add9e154cd0939b7e0cb72f9cfe0acc6c6
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-04 10:06:58 +0300

    Create ICommentService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/ICommentService.cs
@@ -0,0 +1,13 @@
+﻿using News_Reviews.Models.Models.Comments;
+
+namespace News_Reviews.Services.Interfaces
+{
+    public interface ICommentService
+    {
+        public Task<IEnumerable<CommentsViewModel>> GetCommentsAsync(int id);
+
+        public Task AddNewCommentAsync(CommentsFormModel model, string userId);
+
+        public Task RemoveCommentAsync(int id);
+    }
+}
