88a4f5eebbb8c580b14db2518802d9de1dd263dc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-05 12:42:23 +0300

    Update database

---
+++ Project/News&Reviews/News&Reviews.Data/Migrations/20230804090303_UpdateUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News_Reviews.Data.Migrations
+{
+    public partial class UpdateUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Themes",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Themes_ApplicationUserId",
+                table: "Themes",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Themes_AspNetUsers_ApplicationUserId",
+                table: "Themes",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Themes_AspNetUsers_ApplicationUserId",
+                table: "Themes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Themes_ApplicationUserId",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Themes");
+        }
+    }
+}
