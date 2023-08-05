670ab33b47d75cbbeda3e553c896e07ef5c70a49
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-05 12:42:46 +0300

    Update ISearchService.cs

--- Project/News&Reviews/News&Reviews.Services/Interfaces/ISearchService.cs
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/ISearchService.cs
@@ -1,4 +1,5 @@
 ﻿using News_Reviews.DataModels.DataModels;
+using News_Reviews.Models.Models.Forum;
 using News_Reviews.Models.Models.News;
 using News_Reviews.Models.Models.Reviews;
 using System.IO;
@@ -10,5 +11,9 @@ namespace News_Reviews.Services.Interfaces
         Task<IEnumerable<ReviewsViewModel>> SearchReview(string query, IEnumerable<ReviewsViewModel> reviews);
 
         Task<IEnumerable<NewsViewModel>> SearchNews(string query, IEnumerable<NewsViewModel> news);
+
+        Task<IEnumerable<PostViewModel>> SearchPosts(string query, IEnumerable<PostViewModel> posts);
+
+        Task<IEnumerable<ThemesViewModel>> SearchThemes(string query, IEnumerable<ThemesViewModel> themes);
     }
 }
