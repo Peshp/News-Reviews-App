72f607655c44080483c4ce413f54f4425f54bc51
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-05 12:42:40 +0300

    Update ICommentService.cs

--- Project/News&Reviews/News&Reviews.Services/Interfaces/ICommentService.cs
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/ICommentService.cs
@@ -6,6 +6,8 @@ namespace News_Reviews.Services.Interfaces
     {
         public Task<IEnumerable<CommentsViewModel>> GetCommentsAsync(int id);
 
+        public Task<IEnumerable<CommentsViewModel>> GetUserCommentsAsync(string id);
+
         public Task AddNewCommentAsync(CommentsFormModel model, string userId);
 
         public Task RemoveCommentAsync(int id);
