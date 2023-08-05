87b1c8e2bb014b7f2a12bc2984652b8107094358
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-05 12:42:34 +0300

    Create IAccountService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Interfaces/IAccountService.cs
@@ -0,0 +1,22 @@
+﻿using News_Reviews.DataModels;
+using News_Reviews.Models.Models.Account;
+using News_Reviews.Models.Models.Comments;
+using News_Reviews.Models.Models.Forum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News_Reviews.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<IEnumerable<LastActivityViewModel>> GetLastActivityAsync(
+            IEnumerable<CommentsViewModel> comments,
+            IEnumerable<PostViewModel> posts,
+            string id);
+
+        Task<DisplayProfileViewModel> GetUserDetailsAsync(ApplicationUser user);
+    }
+}
