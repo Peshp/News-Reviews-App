83c5627d416932e2fb95f10f8ad94676c35961cc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-05 12:42:48 +0300

    Create AccountService.cs

---
+++ Project/News&Reviews/News&Reviews.Services/Services/AccountService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using News_Reviews.Data;
+using News_Reviews.DataModels;
+using News_Reviews.Models.Models.Account;
+using News_Reviews.Models.Models.Comments;
+using News_Reviews.Models.Models.Forum;
+using News_Reviews.Services.Interfaces;
+using System;
+
+namespace News_Reviews.Services.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly ApplicationDbContext context;
+
+        public AccountService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<LastActivityViewModel>> GetLastActivityAsync(
+            IEnumerable<CommentsViewModel> comments,
+            IEnumerable<PostViewModel> posts,
+            string id)
+        {
+            var models = await context.Users
+                .Where(u => u.Id == id)
+                .Select(u => new LastActivityViewModel
+                {
+                    Comments = comments,
+                    Posts = posts,
+                })
+                .ToArrayAsync();
+
+            return models;
+        }
+
+        public async Task<DisplayProfileViewModel> GetUserDetailsAsync(ApplicationUser user)
+        {
+            DisplayProfileViewModel model = new DisplayProfileViewModel()
+            {
+                UserId = user.Id,
+                Username = user.UserName,
+                Password = user.PasswordHash,
+            };
+
+            return model;
+        }
+    }
+}
