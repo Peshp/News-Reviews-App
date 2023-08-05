14ad2d1ebe6d0950ffb5ada32e19b7ce3831a4e7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-05 12:42:32 +0300

    Create LastActivityViewModel.cs

---
+++ Project/News&Reviews/News&Reviews.Models/Models/Account/LastActivityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using News_Reviews.Models.Models.Comments;
+using News_Reviews.Models.Models.Forum;
+using System;
+
+namespace News_Reviews.Models.Models.Account
+{
+    public class LastActivityViewModel
+    {
+        public IEnumerable<CommentsViewModel> Comments { get; set; }
+
+        public IEnumerable<PostViewModel> Posts { get; set; }
+    }
+}
