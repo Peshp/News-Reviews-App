7d443dec45665eb8f67a03fa3cdd5ffebd113495
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-06 10:19:47 +0300

    Update HomeController.cs

--- Project/News&Reviews/News&Reviews/Controllers/HomeController.cs
+++ Project/News&Reviews/News&Reviews/Controllers/HomeController.cs
@@ -11,16 +11,32 @@ namespace News_Reviews.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IReviewsService reviewsService;
+        private INewsService newsService;
+        private IForumService forumService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+            IReviewsService reviewsService, 
+            INewsService newsService, 
+            IForumService forumService)
         {
             _logger = logger;
+            this.reviewsService = reviewsService;
+            this.newsService = newsService;
+            this.forumService = forumService;
         }
 
 
         public async Task<IActionResult> Index()
         {
-            return View();
+            var model = new HomeModel
+            {
+                Reviews = await reviewsService.GetReviewsAsync(),
+                News = await newsService.GetNewsAsync(),
+                Posts = await forumService.GetPostsAsync(2),
+            };
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
