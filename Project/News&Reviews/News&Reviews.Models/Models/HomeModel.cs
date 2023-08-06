1ef7542c9b5c547da6ae5ee10c773e420391f261
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-06 10:19:31 +0300

    Create HomeModel.cs

---
+++ Project/News&Reviews/News&Reviews.Models/Models/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿using News_Reviews.Models.Models.Forum;
+using News_Reviews.Models.Models.News;
+using News_Reviews.Models.Models.Reviews;
+using System;
+
+namespace News_Reviews.Models.Models
+{
+    public class HomeModel
+    {
+        public IEnumerable<ReviewsViewModel> Reviews { get; set; }
+
+        public IEnumerable<NewsViewModel> News { get; set; }
+
+        public IEnumerable<PostViewModel> Posts { get; set; }
+    }
+}
