41f211f0d284263939108a910a5ead984bf4b3e3
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-09 13:23:36 +0300

    Create BaseAdminController.cs

---
+++ Project/News&Reviews/News&Reviews/Areas/Administration/Controllers/BaseAdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace News_Reviews.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    [Authorize(Roles = "Admin")]
+    public class BaseAdminController : Controller
+    {
+
+    }
+}
