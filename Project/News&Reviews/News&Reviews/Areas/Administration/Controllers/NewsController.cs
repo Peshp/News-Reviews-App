6edbad44eac5990dacf3a07f5bf89d0fc8b60367
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-09 13:23:42 +0300

    Create NewsController.cs

---
+++ Project/News&Reviews/News&Reviews/Areas/Administration/Controllers/NewsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using News_Reviews.Models.Models.News;
+using News_Reviews.Services.Interfaces;
+using System.Data;
+
+namespace News_Reviews.Areas.Administration.Controllers
+{
+    public class NewsController : BaseAdminController
+    {
+        private INewsService service;
+
+        public NewsController(INewsService newsService)
+        {
+            service = newsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var platforms = await service.GetPlatformAsync();
+
+            var model = new NewsFormModel()
+            {
+                Platforms = platforms,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(NewsFormModel model)
+        {
+            var validPlatforms = await service.GetPlatformAsync();
+
+            if (ModelState.IsValid)
+            {
+                model.Platforms = validPlatforms;
+                return View(model);
+            }
+
+            await service.AddNews(model);
+
+            return RedirectToAction("All", "News");
+        }
+    }
+}
