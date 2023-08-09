7338fb2f215496d59641b76abe5350bc5e8b8d9f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-09 13:23:39 +0300

    Create HomeController.cs

---
+++ Project/News&Reviews/News&Reviews/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace News_Reviews.Areas.Administration.Controllers
+{
+    public class HomeController : BaseAdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
